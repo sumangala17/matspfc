e31df62e532c8c5b57f0973f7afc61cd8545729e
Sumangala <sumangala.patki@gmail.com>
2023-09-02 19:41:01 -0400

    work fn: track target-agent pair costs with matrix, update agent_path lists with matrix minimum, LOOP

--- libmcpf/heuristics.py
+++ libmcpf/heuristics.py
@@ -1,3 +1,5 @@
+import queue
+
 import numpy as np
 import libmcpf.common as cm
 
@@ -26,15 +28,18 @@ class PathHeuristic:
         #     return -1
         v_in_spmat = self.N + v
         ag_dest_spmat = self.N + self.M + agent_id
+
+        # empty list, just add target between start and destination
         if not self.agent_path[agent_id]:
             original_cost = self.cost_matrix[agent_id][ag_dest_spmat]
             new_cost = self.cost_matrix[agent_id][v_in_spmat] + self.cost_matrix[v_in_spmat][ag_dest_spmat]
-            index = 1
-            return new_cost - original_cost, index
+            return new_cost - original_cost, 0
 
         min_dist = self.cost_matrix[agent_id][v_in_spmat]
         closest_point_on_path = agent_id
         closest_idx = -1
+
+        # find the target in existing agent_path target list closest to the given target
         for i in range(len(self.agent_path[agent_id])):
             target_id = self.agent_path[agent_id][i]
             current_dist = self.cost_matrix[self.N + target_id][v_in_spmat]
@@ -42,18 +47,19 @@ class PathHeuristic:
                 min_dist = current_dist
                 closest_point_on_path = self.N + target_id
                 closest_idx = i
-        current_dist = self.cost_matrix[v_in_spmat][ag_dest_spmat]  # v to destination
+
+        # check if v to destination is closer than v to any target
+        current_dist = self.cost_matrix[v_in_spmat][ag_dest_spmat]
         if current_dist < min_dist:
             original_cost = self.cost_matrix[self.N + self.agent_path[agent_id][-1]][ag_dest_spmat]
             new_cost = self.cost_matrix[self.N + self.agent_path[agent_id][-1]][v_in_spmat] + current_dist
-            index = -1
-            return new_cost - original_cost, index
+            return new_cost - original_cost, -1
 
-        if closest_point_on_path == agent_id:  # closest point is start point
+        # closest point is start point
+        if closest_point_on_path == agent_id:
             original_cost = self.cost_matrix[agent_id][self.N + self.agent_path[agent_id][0]]
             new_cost = min_dist + self.cost_matrix[v_in_spmat][self.N + self.agent_path[agent_id][0]]
-            index = 1
-            return new_cost - original_cost, index
+            return new_cost - original_cost, 0
 
 
         next_to_closest = self.N + self.agent_path[agent_id][closest_idx + 1]
@@ -64,21 +70,56 @@ class PathHeuristic:
                                 - self.cost_matrix[prev_to_closest][closest_point_on_path])
 
         if delta_cost_with_next >= delta_cost_with_prev:
-            return delta_cost_with_prev, (prev_to_closest, closest_idx)
+            return delta_cost_with_prev, closest_idx
         else:
-            return delta_cost_with_next, (closest_idx, next_to_closest)
-        
+            return delta_cost_with_next, closest_idx + 1
 
 
-    def get_agent_list_for_target(self, v):
-        mincost = 0
-        allowed_agents = np.arange(self.N) if v not in self.ac_dict else self.ac_dict[v]
-        for i in range(self.N):
-            cost =
+
+    # def get_agent_list_for_target(self, v):
+    #     mincost = 0
+    #     target_to_agent_path_cost = {}
+    #     allowed_agents = np.arange(self.N) if v not in self.ac_dict else self.ac_dict[v]
+    #     for i in range(self.N):
+    #         if i not in allowed_agents:
+    #             continue
+    #         cost, idx = self.find_delta_cost_after_adding_target(i, v)
+    #         target_to_agent_path_cost[(v, i)] = [cost, p1, p2]
 
     def work(self):
-        for v in self.targets:
-            sorted_agents = self.get_agent_list_for_target()
+        # initialize static matrix
+        target_agent_mat = np.ones(self.M, self.N) * -1
+        for i in range(self.M):
+            if self.targets[i] not in self.ac_dict:
+                target_agent_mat[self.targets[i]] = 0
+                continue
+            for j in range(self.N):
+                if j in self.ac_dict[self.targets[i]]:
+                    target_agent_mat = 0
+
+        coord_dict = {}
+        # populate static matrix
+        for i in range(self.N):
+            for j in range(self.M):
+                if target_agent_mat[i][j] == -1:
+                    continue
+                cost, index = self.find_delta_cost_after_adding_target(i, j)
+                target_agent_mat[i][j] = cost
+                coord_dict[(i, j)] = index
+
+        visited_targets = set()
+        while len(visited_targets) < self.M:
+            # find the least cost target-agent pair and insert into agent_path for agent
+            agent, target = np.unravel_index(target_agent_mat.argmin(), target_agent_mat.shape)
+            self.agent_path[agent].insert(coord_dict[(agent, target)], target)
+            visited_targets.add(target)
+            # update the target-agent matrix for the given agent whose path was modified
+            for j in range(self.M):
+                if target_agent_mat[agent][j] == -1:
+                    continue
+                cost, index = self.find_delta_cost_after_adding_target(i, j)
+                target_agent_mat[i][j] = cost
+                coord_dict[(i, j)] = index
 
 
 
